8e43229f39280b5f096b21701c134cff0df8f400
Thiago S. Santos <ssthiago.dev@gmail.com>
2023-06-03 16:11:57 -0300

    Add list and create user in memory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((request, response) => {
-  return response.end('Hello World!');
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+    });
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end();
 });
 
 server.listen(3333);
